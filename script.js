9a2f0ef170704ae5c56b606c0e086cec38d4a5c9
snehal-mane <snehal6mane@gmail.com>
2020-08-07 14:17:34 +0530

    Row win logic added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //first x, then o
 var x_o = "1"; //if 1 -> x, -1 -> o
-var box = [['','',''],['','',''],['','','']]; //true = occupied, box[row][column]
+var box = [['0','0','0'],['0','0','0'],['0','0','0']]; //true = occupied, box[row][column]
 
 $(document).ready(function() {
     $('.ttt-box').click(function() {
@@ -9,7 +9,7 @@ $(document).ready(function() {
         var column = $(this).attr('id'); //get column
         $('.warning').text(''); //clear warning text
 
-        if(box[row][column]=='') { //if box is empty then proceed
+        if(box[row][column]=='0') { //if box is empty then proceed
             if(x_o == "1") {
                 $(this).text("X"); //set 'X'
                 $('#current').text("O"); //next turn is 'O'
@@ -28,5 +28,17 @@ $(document).ready(function() {
 });
 
 function checkWin() {
-    
+    //check rows - if any row gives 3 or -3, -> win
+    for(let i=0;i<3;i++) {
+            var sum = arraySum(box[i]);
+            if(sum==3 || sum==-3) {
+                $('.warning').text('WIN!!');
+        }
+    }
+    //check columns - if any column gives 3 or -3, -> win
+    //check cross - if any cross gives 3 or -3, -> win
+}
+
+function arraySum(arr) {
+    return arr.reduce((a, b) => Number(a) + Number(b), 0)
 }
