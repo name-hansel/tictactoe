15b46b0ee768adf7b4fbc001370226832ea81b69
snehal-mane <snehal6mane@gmail.com>
2020-08-07 17:05:02 +0530

    Added draw condition; broke up into functions; reload when game is won or draw and user clicks

--- script.js
+++ script.js
@@ -1,47 +1,32 @@
 //first x, then o
-var x_o = "1"; //if 1 -> x, -1 -> o
-var box = [['0','0','0'],['0','0','0'],['0','0','0']]; //true = occupied, box[row][column]
-
+var x_o = 1; //if 1 -> x, -1 -> o
+var box = [[0,0,0],[0,0,0],[0,0,0]]; //true = occupied, box[row][column]
+var nextGame = 0;
+var win = "";
 
 $(document).ready(function() {
     $('.ttt-box').click(function() {
-        var row = $(this).parent().attr('id'); //get row
-        var column = $(this).attr('id'); //get column
-        $('.warning').text(''); //clear warning text
-
-        if(box[row][column]=='0') { //if box is empty then proceed
-            if(x_o == "1") {
-                $(this).text("X"); //set 'X'
-                $('#current').text("O"); //next turn is 'O'
-                box[row][column] = "1"; //update value in box array
-            } else {
-                $(this).text("O"); //set 'O'
-                $('#current').text("X"); //next turn is 'X'
-                box[row][column] = "-1"; //update value in box array
-            }
-
-            if(checkWin()) { //check if anyone wins
-                $('.mainheading').css('color', 'red'); //change color of mainheading to red
-                $('#next').hide();
-                $('#current').hide();
-                $('.reset').text('Click to reset');
-
-                if(x_o == 1) { //current player is X 
-                    $('.mainheading').text('X WINS!');
-                } else { //current player is O
-                    $('.mainheading').text('O WINS!');
+        if(nextGame!=1) {
+            var row = $(this).parent().attr('id'); //get row
+            var column = $(this).attr('id'); //get column
+            $('.warning').text(''); //clear warning text
+            if(box[row][column]==0) {
+                //box is empty
+                addSymbol(this,row,column);
+                //check if anyone wins
+                if(checkWin()) { 
+                    gameWon();
+                } else {
+                    x_o *= -1; //change player
                 }
-
-            } else {
-                x_o *= -1; //change player
+            } else { //box is not empty
+                $('.warning').text('TRY AGAIN'); //user clicks on occupied box
             }
-
-        } else {
-            $('.warning').text('TRY AGAIN'); //user clicks on occupied box
+            isDraw(); //check if draw
+        } else { //draw or won and user clicks
+            location.reload();
         }
     });
-
-
     $('.reset').click(function() {
         location.reload();
     });
@@ -50,31 +35,80 @@ $(document).ready(function() {
 
 function checkWin() {
         //check left to right cross - if any cross gives 3 or -3, -> win
-        var sum = Number(box[0][0])+Number(box[1][1])+Number(box[2][2]);
+        var sum = box[0][0]+box[1][1]+box[2][2];
         if(sum==3 || sum==-3) {
+            win = "lcross";
             return 1;
         }
 
         //check right to left cross - if any cross gives 3 or -3, -> win
-        sum = Number(box[0][2])+Number(box[1][1])+Number(box[2][0]);
+        sum = box[0][2]+box[1][1]+box[2][0];
         if(sum==3 || sum==-3) {
+
             return 1;
         }
 
         for(let i=0;i<3;i++) {
         //check rows - if any row gives 3 or -3, -> win
-        sum = arraySum(box[i]);
-        if(sum==3 || sum==-3) {
-            return 1;
-        }
+            sum = arraySum(box[i]);
+            if(sum==3 || sum==-3) {
+                return 1;
+            }
         //check columns - if any column gives 3 or -3, -> win
-        sum=Number(box[0][i])+Number(box[1][i])+Number(box[2][i]);
-        if(sum==3 || sum==-3) {
-            return 1;
-        }   
-    }
+            sum=box[0][i]+box[1][i]+box[2][i];
+            if(sum==3 || sum==-3) {
+                return 1;
+            }   
+        }
+
+    return 0;
 }
 
 function arraySum(arr) {
-    return arr.reduce((a, b) => Number(a) + Number(b), 0)
+    return arr.reduce((a, b) =>a + b, 0)
+}
+
+function gameWon() {
+    nextGame = 1;
+    $('.mainheading').css('color', 'red'); //change color of mainheading to red
+    $('#next').hide();
+    $('#current').hide();
+    $('.reset').text('Click to reset');
+    if(x_o == 1) { //current player is X 
+        $('.mainheading').text('X WINS!');
+    } else { //current player is O
+        $('.mainheading').text('O WINS!');
+    }
+    //line here
+
+    $('.ttt-box').click(function() { //reload page if user clicks after winning
+        location.reload();
+    });
+}
+
+function addSymbol(x,row,column) {
+    if(x_o == "1") {
+        $(x).text("X"); //set 'X'
+        $('#current').text("O"); //next turn is 'O'
+        box[row][column] = 1; //update value in box array
+    } else {
+        $(x).text("O"); //set 'O'
+        $('#current').text("X"); //next turn is 'X'
+        box[row][column] = -1; //update value in box array
+    }
+}
+
+function isDraw() {
+    var include = false;
+    for(let i=0;i<3;i++) {
+        if(box[i].includes(0)) {
+            include = true;
+        }
+    }
+    if(!include) {
+        nextGame = 1;
+        $('.mainheading').text('DRAW!');
+        $('.mainheading').css('color', 'red');
+    }
+    return;
 }
