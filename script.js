8045d28b21e692f64e9b3f6e6816c6302bc5884e
snehal-mane <snehal6mane@gmail.com>
2020-08-07 13:46:05 +0530

    Changed the numbering of the boxes from 0-8 to row and column wise by adding a parent div element whose id gives the row number

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
-/*var box0 = document.querySelector.bind('#box0')
-var box1 = document.querySelector.bind('#box1')
-var box2 = document.querySelector.bind('#box2')
-var box3 = document.querySelector.bind('#box3')
-var box4 = document.querySelector.bind('#box4')
-var box5 = document.querySelector.bind('#box5')
-var box6 = document.querySelector.bind('#box6')
-var box7 = document.querySelector.bind('#box7')
-var box8 = document.querySelector.bind('#box8')*/
-
 //first x, then o
 var x_o = "1"; //if 1 -> x, -1 -> o
-var box = [['empty','empty','empty','empty','empty','empty','empty','empty','empty'], ['empty','empty','empty','empty','empty','empty','empty','empty','empty']]; //true = occupied
-
+var box = [['','',''],['','',''],['','','']]; //true = occupied, box[row][column]
 
 $(document).ready(function() {
     $('.ttt-box').click(function() {
-        var clicked = $(this).attr('id'); //get boxid
-        $('.warning').text('');
-        if(box[0][clicked]=='empty' && box[1][clicked]=='empty') {
+
+        var row = $(this).parent().attr('id'); //get row
+        var column = $(this).attr('id'); //get column
+        $('.warning').text(''); //clear warning text
+
+        if(box[row][column]=='') { //if box is empty then proceed
             if(x_o == "1") {
-                $(this).text("X");
-                $('#current').text("O");
-                box[0][clicked] = true;
+                $(this).text("X"); //set 'X'
+                $('#current').text("O"); //next turn is 'O'
+                box[row][column] = "1"; //update value in box array
             } else {
-                $(this).text("O");
-                $('#current').text("X");
-                box[1][clicked] = true;
+                $(this).text("O"); //set 'O'
+                $('#current').text("X"); //next turn is 'X'
+                box[row][column] = "-1"; //update value in box array
             }
-            x_o *= -1;
-            checkWin();
+            x_o *= -1; //change player
+            checkWin(); //check if someone wins 
         } else {
-            $('.warning').text('TRY AGAIN');
+            $('.warning').text('TRY AGAIN'); //user clicks on occupied box
         }
     });
 });
