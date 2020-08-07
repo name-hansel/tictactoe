c3961ac51687b952cca71b36eccb005da939358f
snehal-mane <snehal6mane@gmail.com>
2020-08-07 16:06:45 +0530

    Added logic for column and cross wins

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 var x_o = "1"; //if 1 -> x, -1 -> o
 var box = [['0','0','0'],['0','0','0'],['0','0','0']]; //true = occupied, box[row][column]
 
+
 $(document).ready(function() {
     $('.ttt-box').click(function() {
-
         var row = $(this).parent().attr('id'); //get row
         var column = $(this).attr('id'); //get column
         $('.warning').text(''); //clear warning text
@@ -19,24 +19,60 @@ $(document).ready(function() {
                 $('#current').text("X"); //next turn is 'X'
                 box[row][column] = "-1"; //update value in box array
             }
-            x_o *= -1; //change player
-            checkWin(); //check if someone wins 
+
+            if(checkWin()) { //check if anyone wins
+                $('.mainheading').css('color', 'red'); //change color of mainheading to red
+                $('#next').hide();
+                $('#current').hide();
+                $('.reset').text('Click to reset');
+
+                if(x_o == 1) { //current player is X 
+                    $('.mainheading').text('X WINS!');
+                } else { //current player is O
+                    $('.mainheading').text('O WINS!');
+                }
+
+            } else {
+                x_o *= -1; //change player
+            }
+
         } else {
             $('.warning').text('TRY AGAIN'); //user clicks on occupied box
         }
     });
+
+
+    $('.reset').click(function() {
+        location.reload();
+    });
 });
 
+
 function checkWin() {
-    //check rows - if any row gives 3 or -3, -> win
-    for(let i=0;i<3;i++) {
-            var sum = arraySum(box[i]);
-            if(sum==3 || sum==-3) {
-                $('.warning').text('WIN!!');
+        //check left to right cross - if any cross gives 3 or -3, -> win
+        var sum = Number(box[0][0])+Number(box[1][1])+Number(box[2][2]);
+        if(sum==3 || sum==-3) {
+            return 1;
         }
+
+        //check right to left cross - if any cross gives 3 or -3, -> win
+        sum = Number(box[0][2])+Number(box[1][1])+Number(box[2][0]);
+        if(sum==3 || sum==-3) {
+            return 1;
+        }
+
+        for(let i=0;i<3;i++) {
+        //check rows - if any row gives 3 or -3, -> win
+        sum = arraySum(box[i]);
+        if(sum==3 || sum==-3) {
+            return 1;
+        }
+        //check columns - if any column gives 3 or -3, -> win
+        sum=Number(box[0][i])+Number(box[1][i])+Number(box[2][i]);
+        if(sum==3 || sum==-3) {
+            return 1;
+        }   
     }
-    //check columns - if any column gives 3 or -3, -> win
-    //check cross - if any cross gives 3 or -3, -> win
 }
 
 function arraySum(arr) {
